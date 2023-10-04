8bf2861ad7492ef4f90b4481bef978ee2586e522
Daniel Sant'Anna <danielcarlossantanna@gmail.com>
2023-10-03 23:27:36 -0300

    test: 💍 add tests to button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,18 +1,18 @@
 import { screen } from "@testing-library/react";
 
-import Card from ".";
+import Button from ".";
 import renderWithProviders from "helpers/renderWithProviders";
 
-describe("<Card />", () => {
+describe("<Button />", () => {
   it("Should render children text", () => {
     const text = "text_test";
-    renderWithProviders(<Card>{text}</Card>);
+    renderWithProviders(<Button>{text}</Button>);
     expect(screen.getByText(text)).toBeInTheDocument();
   });
 
   it("Should render children text with fontSize sent", () => {
     const text = "text_test";
-    renderWithProviders(<Card>{text}</Card>);
+    renderWithProviders(<Button>{text}</Button>);
     expect(screen.getByText(text)).toBeInTheDocument();
   });
 });
