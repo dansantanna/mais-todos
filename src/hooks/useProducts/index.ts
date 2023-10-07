8c51d0e7d85f4dc3a740fbc3a399077fa16a7c09
Daniel Sant'Anna <danielcarlossantanna@gmail.com>
2023-10-07 18:49:44 -0300

    refactor: 💡 remove unnecessary hook

--- src/hooks/useProducts/index.ts
+++ src/hooks/useProducts/index.ts
@@ -1,21 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 import { fetchProducts } from "services/fakeStore";
 import IProduct from "types/IProduct";
 
 const useProducts = (id?: IProduct["id"]) => {
-  const [products, setProducts] = useState<IProduct[]>([]);
-
-  const { data, isLoading, isError } = useQuery({
+  const {
+    data: products,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["products", id],
     queryFn: () => fetchProducts(id),
   });
 
-  useEffect(() => {
-    setProducts(data ?? []);
-  }, [data]);
-
-  return { products, isLoading, isError };
+  return { products: products ?? [], isLoading, isError };
 };
 
 export default useProducts;
